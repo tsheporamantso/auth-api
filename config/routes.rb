e9656ab733f8bddf3fdf4acdb0e9cc36dc4f0029
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-27 15:04:36 +0200

    Add get route for members controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"}
+  get 'members', to: 'members#index'
 end
