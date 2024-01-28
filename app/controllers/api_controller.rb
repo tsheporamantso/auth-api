e9b906225ef1937d11b936d8e89969fd4874f3b4
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-28 21:56:08 +0200

    Customise authorization error message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,7 @@
 class ApiController < ApplicationController
     before_action :authenticate_user!
+
+    rescue_from CanCan::AccessDenied do |exception|
+      render json: { warning: exception, status: "authorization_failed" }
+    end
 end
